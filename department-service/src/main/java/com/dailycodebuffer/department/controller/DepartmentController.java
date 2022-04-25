ae3fc39c9b0fa648c0ac9be99e7145860f890f35
Dan <mazinadan2@gmail.com>
2022-04-25 11:48:44 +0200

    Added Swagger

--- department-service/src/main/java/com/dailycodebuffer/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/dailycodebuffer/department/controller/DepartmentController.java
@@ -2,6 +2,10 @@ package com.dailycodebuffer.department.controller;
 
 import com.dailycodebuffer.department.model.Department;
 import com.dailycodebuffer.department.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +20,43 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
     Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+    @Operation(summary = "This is to add a new Department to Db")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Department Added to Db",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @PostMapping("/save-department")
     public Department saveDepartment(@RequestBody Department department){
         logger.info("DepartmentController.saveDepartment()");
         return departmentService.saveDepartment(department);
     }
+    @Operation(summary = "This is to fetch a Department from Db")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Fetched Department form Db",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public Department getDepartment(@PathVariable("id") Long departmentId){
         logger.info("DepartmentController.getDepartment()");
         return departmentService.getDepartment(departmentId);
     }
+    @Operation(summary = "This is to fetch All the Departments stored in Db")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Fetched All the Department form Db",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @GetMapping("/allDepartments")
     public List<Department> fetchAllDepartments(){
         return departmentService.fetchAllDepartments();
--- user-service/src/main/java/com/dailycodebuffer/user/controller/UserController.java
+++ user-service/src/main/java/com/dailycodebuffer/user/controller/UserController.java
@@ -3,6 +3,10 @@ package com.dailycodebuffer.user.controller;
 import com.dailycodebuffer.user.VO.ResponseTemplateVo;
 import com.dailycodebuffer.user.model.User;
 import com.dailycodebuffer.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,25 +20,70 @@ public class UserController {
     @Autowired
     private UserService userService;
     Logger logger = LoggerFactory.getLogger(UserController.class);
+    @Operation(summary = "This is to add a new User to Db")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "User Added to Db",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @PostMapping("/saveUser")
     public User createAndSaveUser( @RequestBody User user){
         logger.info("UserController.createAndSaveUser()");
 
         return userService.saveUser(user);
     }
+    @Operation(summary = "This is to get a user in a department")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "User with a specific department Added to Db",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @GetMapping("/getUser/{id}")
     public ResponseTemplateVo getUserWithDepartments(@PathVariable(value = "id") Long userId){
         logger.info("UserController.getUserWithDepartments()");
         return userService.getUserWithDepartments(userId);
     }
+    @Operation(summary = "This is to get a User to Db")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "User Retrieved",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @GetMapping("/getOneUser/{id}")
         public User getOneUser(@PathVariable("id")Long userId){
         return userService.getOneUser(userId);
     }
+    @Operation(summary = "This is to get all Users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Department Retrieved to Db",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @GetMapping("/allUsers")
     public List<User>getAllUsers(){
         return userService.getAllUsers();
     }
+    @Operation(summary = "This is to Deleted a User to Db")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "User Deleted to Db",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "NOt Available",
+                    content = @Content)
+    })
     @PostMapping("/deleteUser/{id}")
     public void deleteUser(@PathVariable("id") Long userId){
         userService.deleteUser(userId);
